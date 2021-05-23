62cc85c955bf3be65a31611ea956a58bae63c608
Javier Spagnoletti <phansys@gmail.com>
2021-05-23 19:55:28 -0300

    First commit

---
+++ tests/BuildTest.php
@@ -0,0 +1,428 @@
+<?php
+
+/*
+ * This file is part of PhpZabbixApi.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @copyright The MIT License (MIT)
+ * @author confirm IT solutions GmbH, Rathausstrase 14, CH-6340 Baar
+ */
+
+namespace ZabbixSdkBuilder\Tests;
+
+use PHPUnit\Framework\TestCase;
+use ZabbixApi\ZabbixApi;
+
+/**
+ * @author Javier Spagnoletti <phansys@gmail.com>
+ */
+final class BuildTest extends TestCase
+{
+    public function testZabbixApiSymbolsCount(): void
+    {
+        $zabbix = new ZabbixApi('https://localhost/json_rpc.php', 'zabbix', 'very_secret');
+        $ro = new \ReflectionObject($zabbix);
+
+        $this->assertGreaterThanOrEqual(360, count($ro->getMethods(\ReflectionMethod::IS_PUBLIC)));
+        $this->assertGreaterThanOrEqual(668, count($ro->getConstants()));
+    }
+
+    /**
+     * @dataProvider provideMethodNames
+     */
+    public function testZabbixApiMethods(string $methodName): void
+    {
+        $this->assertTrue(method_exists(ZabbixApi::class, $methodName));
+    }
+
+    public function provideMethodNames(): iterable
+    {
+        yield ['apiTableName'];
+        yield ['apiPk'];
+        yield ['apiPkOption'];
+        yield ['actionGet'];
+        yield ['actionCreate'];
+        yield ['actionUpdate'];
+        yield ['actionDelete'];
+        yield ['actionValidateOperationsIntegrity'];
+        yield ['actionValidateOperationConditions'];
+        yield ['actionTableName'];
+        yield ['actionPk'];
+        yield ['actionPkOption'];
+        yield ['alertGet'];
+        yield ['alertTableName'];
+        yield ['alertPk'];
+        yield ['alertPkOption'];
+        yield ['apiinfoVersion'];
+        yield ['apiinfoTableName'];
+        yield ['apiinfoPk'];
+        yield ['apiinfoPkOption'];
+        yield ['applicationGet'];
+        yield ['applicationCheckInput'];
+        yield ['applicationCreate'];
+        yield ['applicationUpdate'];
+        yield ['applicationDelete'];
+        yield ['applicationMassAdd'];
+        yield ['applicationTableName'];
+        yield ['applicationPk'];
+        yield ['applicationPkOption'];
+        yield ['configurationExport'];
+        yield ['configurationImport'];
+        yield ['configurationTableName'];
+        yield ['configurationPk'];
+        yield ['configurationPkOption'];
+        yield ['dcheckGet'];
+        yield ['dcheckIsReadable'];
+        yield ['dcheckIsWritable'];
+        yield ['dcheckTableName'];
+        yield ['dcheckPk'];
+        yield ['dcheckPkOption'];
+        yield ['dhostGet'];
+        yield ['dhostTableName'];
+        yield ['dhostPk'];
+        yield ['dhostPkOption'];
+        yield ['discoveryruleGet'];
+        yield ['discoveryruleCreate'];
+        yield ['discoveryruleUpdate'];
+        yield ['discoveryruleDelete'];
+        yield ['discoveryruleCopy'];
+        yield ['discoveryruleSyncTemplates'];
+        yield ['discoveryruleIsReadable'];
+        yield ['discoveryruleIsWritable'];
+        yield ['discoveryruleFindInterfaceForItem'];
+        yield ['discoveryruleTableName'];
+        yield ['discoveryrulePk'];
+        yield ['discoveryrulePkOption'];
+        yield ['druleGet'];
+        yield ['druleCheckInput'];
+        yield ['druleCreate'];
+        yield ['druleUpdate'];
+        yield ['druleDelete'];
+        yield ['druleIsReadable'];
+        yield ['druleIsWritable'];
+        yield ['druleTableName'];
+        yield ['drulePk'];
+        yield ['drulePkOption'];
+        yield ['dserviceGet'];
+        yield ['dserviceTableName'];
+        yield ['dservicePk'];
+        yield ['dservicePkOption'];
+        yield ['eventGet'];
+        yield ['eventAcknowledge'];
+        yield ['eventTableName'];
+        yield ['eventPk'];
+        yield ['eventPkOption'];
+        yield ['graphGet'];
+        yield ['graphSyncTemplates'];
+        yield ['graphDelete'];
+        yield ['graphUpdate'];
+        yield ['graphCreate'];
+        yield ['graphTableName'];
+        yield ['graphPk'];
+        yield ['graphPkOption'];
+        yield ['graphitemGet'];
+        yield ['graphitemTableName'];
+        yield ['graphitemPk'];
+        yield ['graphitemPkOption'];
+        yield ['graphprototypeGet'];
+        yield ['graphprototypeSyncTemplates'];
+        yield ['graphprototypeDelete'];
+        yield ['graphprototypeUpdate'];
+        yield ['graphprototypeCreate'];
+        yield ['graphprototypeTableName'];
+        yield ['graphprototypePk'];
+        yield ['graphprototypePkOption'];
+        yield ['hostGet'];
+        yield ['hostCreate'];
+        yield ['hostUpdate'];
+        yield ['hostMassAdd'];
+        yield ['hostMassUpdate'];
+        yield ['hostMassRemove'];
+        yield ['hostDelete'];
+        yield ['hostIsReadable'];
+        yield ['hostIsWritable'];
+        yield ['hostTableName'];
+        yield ['hostPk'];
+        yield ['hostPkOption'];
+        yield ['hostgroupGet'];
+        yield ['hostgroupCreate'];
+        yield ['hostgroupUpdate'];
+        yield ['hostgroupDelete'];
+        yield ['hostgroupMassAdd'];
+        yield ['hostgroupMassRemove'];
+        yield ['hostgroupMassUpdate'];
+        yield ['hostgroupIsReadable'];
+        yield ['hostgroupIsWritable'];
+        yield ['hostgroupTableName'];
+        yield ['hostgroupPk'];
+        yield ['hostgroupPkOption'];
+        yield ['hostprototypeGet'];
+        yield ['hostprototypeCreate'];
+        yield ['hostprototypeUpdate'];
+        yield ['hostprototypeSyncTemplates'];
+        yield ['hostprototypeDelete'];
+        yield ['hostprototypeIsReadable'];
+        yield ['hostprototypeIsWritable'];
+        yield ['hostprototypeTableName'];
+        yield ['hostprototypePk'];
+        yield ['hostprototypePkOption'];
+        yield ['historyGet'];
+        yield ['historyTableName'];
+        yield ['historyPk'];
+        yield ['historyPkOption'];
+        yield ['hostinterfaceGet'];
+        yield ['hostinterfaceCheckInput'];
+        yield ['hostinterfaceCreate'];
+        yield ['hostinterfaceUpdate'];
+        yield ['hostinterfaceDelete'];
+        yield ['hostinterfaceMassAdd'];
+        yield ['hostinterfaceMassRemove'];
+        yield ['hostinterfaceReplaceHostInterfaces'];
+        yield ['hostinterfaceTableName'];
+        yield ['hostinterfacePk'];
+        yield ['hostinterfacePkOption'];
+        yield ['imageGet'];
+        yield ['imageCreate'];
+        yield ['imageUpdate'];
+        yield ['imageDelete'];
+        yield ['imageTableName'];
+        yield ['imagePk'];
+        yield ['imagePkOption'];
+        yield ['iconmapGet'];
+        yield ['iconmapCreate'];
+        yield ['iconmapUpdate'];
+        yield ['iconmapDelete'];
+        yield ['iconmapIsReadable'];
+        yield ['iconmapIsWritable'];
+        yield ['iconmapTableName'];
+        yield ['iconmapPk'];
+        yield ['iconmapPkOption'];
+        yield ['itemGet'];
+        yield ['itemCreate'];
+        yield ['itemUpdate'];
+        yield ['itemDelete'];
+        yield ['itemSyncTemplates'];
+        yield ['itemValidateInventoryLinks'];
+        yield ['itemAddRelatedObjects'];
+        yield ['itemFindInterfaceForItem'];
+        yield ['itemIsReadable'];
+        yield ['itemIsWritable'];
+        yield ['itemTableName'];
+        yield ['itemPk'];
+        yield ['itemPkOption'];
+        yield ['itemprototypeGet'];
+        yield ['itemprototypeCreate'];
+        yield ['itemprototypeUpdate'];
+        yield ['itemprototypeDelete'];
+        yield ['itemprototypeSyncTemplates'];
+        yield ['itemprototypeAddRelatedObjects'];
+        yield ['itemprototypeFindInterfaceForItem'];
+        yield ['itemprototypeIsReadable'];
+        yield ['itemprototypeIsWritable'];
+        yield ['itemprototypeTableName'];
+        yield ['itemprototypePk'];
+        yield ['itemprototypePkOption'];
+        yield ['maintenanceGet'];
+        yield ['maintenanceCreate'];
+        yield ['maintenanceUpdate'];
+        yield ['maintenanceDelete'];
+        yield ['maintenanceTableName'];
+        yield ['maintenancePk'];
+        yield ['maintenancePkOption'];
+        yield ['mapGet'];
+        yield ['mapCreate'];
+        yield ['mapUpdate'];
+        yield ['mapDelete'];
+        yield ['mapIsReadable'];
+        yield ['mapIsWritable'];
+        yield ['mapCheckCircleSelementsLink'];
+        yield ['mapTableName'];
+        yield ['mapPk'];
+        yield ['mapPkOption'];
+        yield ['mediatypeGet'];
+        yield ['mediatypeCreate'];
+        yield ['mediatypeUpdate'];
+        yield ['mediatypeDelete'];
+        yield ['mediatypeTableName'];
+        yield ['mediatypePk'];
+        yield ['mediatypePkOption'];
+        yield ['proxyGet'];
+        yield ['proxyCreate'];
+        yield ['proxyUpdate'];
+        yield ['proxyDelete'];
+        yield ['proxyIsReadable'];
+        yield ['proxyIsWritable'];
+        yield ['proxyTableName'];
+        yield ['proxyPk'];
+        yield ['proxyPkOption'];
+        yield ['serviceGet'];
+        yield ['serviceCreate'];
+        yield ['serviceValidateUpdate'];
+        yield ['serviceUpdate'];
+        yield ['serviceValidateDelete'];
+        yield ['serviceDelete'];
+        yield ['serviceAddDependencies'];
+        yield ['serviceDeleteDependencies'];
+        yield ['serviceValidateAddTimes'];
+        yield ['serviceAddTimes'];
+        yield ['serviceGetSla'];
+        yield ['serviceDeleteTimes'];
+        yield ['serviceIsReadable'];
+        yield ['serviceIsWritable'];
+        yield ['serviceTableName'];
+        yield ['servicePk'];
+        yield ['servicePkOption'];
+        yield ['screenGet'];
+        yield ['screenCreate'];
+        yield ['screenUpdate'];
+        yield ['screenDelete'];
+        yield ['screenTableName'];
+        yield ['screenPk'];
+        yield ['screenPkOption'];
+        yield ['screenitemGet'];
+        yield ['screenitemCreate'];
+        yield ['screenitemUpdate'];
+        yield ['screenitemUpdateByPosition'];
+        yield ['screenitemDelete'];
+        yield ['screenitemIsReadable'];
+        yield ['screenitemIsWritable'];
+        yield ['screenitemTableName'];
+        yield ['screenitemPk'];
+        yield ['screenitemPkOption'];
+        yield ['scriptGet'];
+        yield ['scriptCreate'];
+        yield ['scriptUpdate'];
+        yield ['scriptDelete'];
+        yield ['scriptExecute'];
+        yield ['scriptGetScriptsByHosts'];
+        yield ['scriptTableName'];
+        yield ['scriptPk'];
+        yield ['scriptPkOption'];
+        yield ['templatePkOption'];
+        yield ['templateGet'];
+        yield ['templateCreate'];
+        yield ['templateUpdate'];
+        yield ['templateDelete'];
+        yield ['templateMassAdd'];
+        yield ['templateMassUpdate'];
+        yield ['templateMassRemove'];
+        yield ['templateIsReadable'];
+        yield ['templateIsWritable'];
+        yield ['templateTableName'];
+        yield ['templatePk'];
+        yield ['templatescreenGet'];
+        yield ['templatescreenCopy'];
+        yield ['templatescreenUpdate'];
+        yield ['templatescreenCreate'];
+        yield ['templatescreenDelete'];
+        yield ['templatescreenTableName'];
+        yield ['templatescreenPk'];
+        yield ['templatescreenPkOption'];
+        yield ['templatescreenitemGet'];
+        yield ['templatescreenitemTableName'];
+        yield ['templatescreenitemPk'];
+        yield ['templatescreenitemPkOption'];
+        yield ['trendGet'];
+        yield ['trendTableName'];
+        yield ['trendPk'];
+        yield ['trendPkOption'];
+        yield ['triggerGet'];
+        yield ['triggerCheckInput'];
+        yield ['triggerCreate'];
+        yield ['triggerUpdate'];
+        yield ['triggerDelete'];
+        yield ['triggerAddDependencies'];
+        yield ['triggerDeleteDependencies'];
+        yield ['triggerSyncTemplates'];
+        yield ['triggerSyncTemplateDependencies'];
+        yield ['triggerIsReadable'];
+        yield ['triggerIsWritable'];
+        yield ['triggerTableName'];
+        yield ['triggerPk'];
+        yield ['triggerPkOption'];
+        yield ['triggerprototypeGet'];
+        yield ['triggerprototypeCreate'];
+        yield ['triggerprototypeUpdate'];
+        yield ['triggerprototypeDelete'];
+        yield ['triggerprototypeAddDependencies'];
+        yield ['triggerprototypeSyncTemplateDependencies'];
+        yield ['triggerprototypeSyncTemplates'];
+        yield ['triggerprototypeTableName'];
+        yield ['triggerprototypePk'];
+        yield ['triggerprototypePkOption'];
+        yield ['userGet'];
+        yield ['userCreate'];
+        yield ['userUpdate'];
+        yield ['userUpdateProfile'];
+        yield ['userDelete'];
+        yield ['userAddMedia'];
+        yield ['userUpdateMedia'];
+        yield ['userDeleteMedia'];
+        yield ['userDeleteMediaReal'];
+        yield ['userCheckAuthentication'];
+        yield ['userIsReadable'];
+        yield ['userIsWritable'];
+        yield ['userTableName'];
+        yield ['userPk'];
+        yield ['userPkOption'];
+        yield ['usergroupGet'];
+        yield ['usergroupCreate'];
+        yield ['usergroupUpdate'];
+        yield ['usergroupMassAdd'];
+        yield ['usergroupMassUpdate'];
+        yield ['usergroupDelete'];
+        yield ['usergroupIsReadable'];
+        yield ['usergroupIsWritable'];
+        yield ['usergroupTableName'];
+        yield ['usergroupPk'];
+        yield ['usergroupPkOption'];
+        yield ['usermacroGet'];
+        yield ['usermacroCreateGlobal'];
+        yield ['usermacroUpdateGlobal'];
+        yield ['usermacroDeleteGlobal'];
+        yield ['usermacroCreate'];
+        yield ['usermacroUpdate'];
+        yield ['usermacroDelete'];
+        yield ['usermacroReplaceMacros'];
+        yield ['usermacroTableName'];
+        yield ['usermacroPk'];
+        yield ['usermacroPkOption'];
+        yield ['usermediaGet'];
+        yield ['usermediaTableName'];
+        yield ['usermediaPk'];
+        yield ['usermediaPkOption'];
+        yield ['valuemapGet'];
+        yield ['valuemapCreate'];
+        yield ['valuemapUpdate'];
+        yield ['valuemapDelete'];
+        yield ['valuemapTableName'];
+        yield ['valuemapPk'];
+        yield ['valuemapPkOption'];
+        yield ['httptestGet'];
+        yield ['httptestCreate'];
+        yield ['httptestUpdate'];
+        yield ['httptestDelete'];
+        yield ['httptestIsReadable'];
+        yield ['httptestIsWritable'];
+        yield ['httptestTableName'];
+        yield ['httptestPk'];
+        yield ['httptestPkOption'];
+    }
+}
---
+++ tests/ZabbixApiTest.php
@@ -0,0 +1,200 @@
+<?php
+
+/*
+ * This file is part of PhpZabbixApi.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @copyright The MIT License (MIT)
+ * @author confirm IT solutions GmbH, Rathausstrase 14, CH-6340 Baar
+ */
+
+namespace ZabbixApi\Tests;
+
+use PHPUnit\Framework\TestCase;
+use ZabbixApi\Exception;
+use ZabbixApi\ZabbixApi;
+
+/**
+ * @author Javier Spagnoletti <phansys@gmail.com>
+ */
+final class ZabbixApiTest extends TestCase
+{
+    public function testZabbixApiClass(): void
+    {
+        $this->assertTrue(class_exists(ZabbixApi::class));
+
+        $this->assertGreaterThanOrEqual(0, version_compare(ZabbixApi::ZABBIX_VERSION, '3.0'));
+
+        $zabbix = new ZabbixApi('http://localhost/json_rpc.php', 'zabbix', 'very_secret');
+
+        $defaultParams = array(
+            'some_param' => array('one'),
+        );
+        $zabbix->setDefaultParams($defaultParams);
+        $this->assertSame('http://localhost/json_rpc.php', $zabbix->getApiUrl());
+        $this->assertSame($defaultParams, $zabbix->getDefaultParams());
+        $this->assertSame('', $zabbix->getRequest());
+        $this->assertSame('', $zabbix->getResponse());
+    }
+
+    public function testUserLoginOnConsecutiveCalls(): void
+    {
+        $user = 'zabbix';
+        $pass = 'very_secret';
+        $authToken = '4u7ht0k3n';
+        $cacheDir = __DIR__.'/.token_cache';
+
+        $this->createTokenCacheDir($cacheDir);
+
+        $zabbix = $this->getMockBuilder(ZabbixApi::class)
+            ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->disableArgumentCloning()
+            ->setMethods(array('request', 'userGet'))
+            ->getMock();
+
+        // `userGet()` must not be called if the argument 3 (`$tokenCacheDir`) is passed with value `null`.
+        $zabbix
+            ->expects($this->never())
+            ->method('userGet')
+            ->with(array('countOutput' => true));
+
+        // `request()` must be called in order to retrieve the token.
+        $zabbix
+            ->expects($this->once())
+            ->method('request')
+            ->with('user.login')
+            ->willReturn($authToken);
+
+        $this->assertSame($authToken, $zabbix->userLogin(array('user' => $user, 'password' => $pass), '', null));
+
+        $zabbix = $this->getMockBuilder(ZabbixApi::class)
+            ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->disableArgumentCloning()
+            ->setMethods(array('request', 'userGet'))
+            ->getMock();
+
+        // `userGet()` must not be called since the token cache file is not created yet.
+        $zabbix
+            ->expects($this->never())
+            ->method('userGet')
+            ->with(array('countOutput' => true));
+
+        // `request()` must be called in order to retrieve the token.
+        $zabbix
+            ->expects($this->once())
+            ->method('request')
+            ->with('user.login')
+            ->willReturn($authToken);
+
+        $this->assertSame($authToken, $zabbix->userLogin(array('user' => $user, 'password' => $pass), '', $cacheDir));
+
+        $zabbix = $this->getMockBuilder(ZabbixApi::class)
+            ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->disableArgumentCloning()
+            ->setMethods(array('request', 'userGet'))
+            ->getMock();
+
+        // `userGet()` must be called since the token at token cache file must be validated.
+        $zabbix
+            ->expects($this->once())
+            ->method('userGet')
+            ->with(array('countOutput' => true));
+
+        // `request()` must not be called since the token was already retrieved from the token cache file.
+        $zabbix
+            ->expects($this->never())
+            ->method('request')
+            ->with('user.login');
+
+        $this->assertSame($authToken, $zabbix->userLogin(array('user' => $user, 'password' => $pass), '', $cacheDir));
+
+        $this->removeTokenCacheDir($cacheDir);
+    }
+
+    /**
+     * @dataProvider getAuthenticationRequired
+     */
+    public function testAuthenticationRequired(string $method, string $apiMethod, bool $isAuthenticationRequired): void
+    {
+        $this->assertTrue(is_callable(array(ZabbixApi::class, $method)));
+
+        $zabbix = $this->getMockBuilder(ZabbixApi::class)
+            ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->disableArgumentCloning()
+            ->setMethods(array('request'))
+            ->getMock();
+
+        $zabbix
+            ->expects($this->once())
+            ->method('request')
+            ->with($apiMethod, array(), '', $isAuthenticationRequired);
+
+        $zabbix->$method();
+    }
+
+    public function getAuthenticationRequired(): iterable
+    {
+        return array(
+            array('method' => 'userGet', 'api_method' => 'user.get', 'is_authentication_required' => true),
+            array('method' => 'apiinfoVersion', 'api_method' => 'apiinfo.version', 'is_authentication_required' => false),
+            array('method' => 'hostGet', 'api_method' => 'host.get', 'is_authentication_required' => true),
+        );
+    }
+
+    public function testZabbixApiConnectionError(): void
+    {
+        $zabbix = new ZabbixApi('https://not.found.tld/json_rpc.php', 'zabbix', 'very_secret');
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Could not connect to "https://not.found.tld/json_rpc.php"');
+
+        $zabbix->userGet();
+    }
+
+    private function createTokenCacheDir(string $cacheDir): void
+    {
+        if (is_dir($cacheDir)) {
+            return;
+        }
+
+        mkdir($cacheDir);
+    }
+
+    private function removeTokenCacheDir(string $cacheDir): void
+    {
+        if (!is_dir($cacheDir)) {
+            return;
+        }
+
+        // Remove the token cache directory.
+        foreach (glob($cacheDir.'/{,.}*', GLOB_BRACE) as $file) {
+            if (is_dir($file)) {
+                continue;
+            }
+            unlink($file);
+        }
+
+        rmdir($cacheDir);
+    }
+}
